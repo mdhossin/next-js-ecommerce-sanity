ef0e834557e79746dab80b34cafa2d847645d74d
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-05-13 19:58:19 +0600

    first commit

---
+++ components/FooterBanner.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { urlFor } from "../lib/client";
+
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  },
+}) => {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+
+        <Image
+          src="/assets/headphones_c_1.webp"
+          className="footer-banner-image"
+          alt=""
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FooterBanner;
